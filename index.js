c9da9eb2136651b2ad9e3bee61955eab14248f95
emilymarro <emily.marro@gmail.com>
2017-09-26 17:35:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-function dwarfRollCall(dwarves) {
+var dwarvesArray = [];
+function dwarfRollCall(dwarvesArray) {
+  for (var i=0; i < dwarvesArray.length; i=i+1 ){
+  var currentDwarfName = dwarvesArray[i];
+  currentDwarfName.toUpperCase())
+}
 }
 
+var planeteerCalls = [];
 function summonCaptainPlanet(planeteerCalls){
+    for (var i=0; i < dwarvesArray.length; i=i+1 ){
+    var currentDwarfName = dwarvesArray[i];
+    currentDwarfName.toUpperCase())
+  }
 }
 
 function longPlaneteerCalls(words) {
